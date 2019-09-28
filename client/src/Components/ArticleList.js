5b6d0bfd545cf6732de406286d742e779b59ad73
cjmcauley <christopher.mcauley@gmail.com>
2019-09-28 14:31:01 +0100

    article list now filtering.

--- client/src/Components/ArticleList.js
+++ client/src/Components/ArticleList.js
@@ -1,17 +1,49 @@
 import React from 'react';
 
 const ArticleList = (props) => {
+  const filter = props.filter;
+
   const articles = props.articles.map((article, index) => {
-    return(
-      <li key={index}>
-      <h2>{article.title}</h2>
-      <h3>{article.author.name}, {article.author.title}</h3>
-      <h3>{article.date}</h3>
-      <h3>{article.category}</h3>
-      <h3>{article.summary}</h3>
-      </li>
-    )
+
+    if(filter.toUpperCase() == article.category){
+      return(
+        <li key={index}>
+        <h2>{article.title}</h2>
+        <h3>{article.author.name}, {article.author.title}</h3>
+        <h3>{article.date}</h3>
+        <h3>{article.category}</h3>
+        <h3>{article.summary}</h3>
+        </li>
+    )}else if(filter == ""){
+      return(
+        <li key={index}>
+        <h2>{article.title}</h2>
+        <h3>{article.author.name}, {article.author.title}</h3>
+        <h3>{article.date}</h3>
+        <h3>{article.category}</h3>
+        <h3>{article.summary}</h3>
+        </li>
+    )}else{
+      return
+  }
   })
+
+// const ArticleList = (props) => {
+//   const filter = props.filter;
+//   const articles = props.articles.map((article, index) => {
+//     return(
+//       <li key={index}>
+//       <h2>{article.title}</h2>
+//       <h3>{article.author.name}, {article.author.title}</h3>
+//       <h3>{article.date}</h3>
+//       <h3>{article.category}</h3>
+//       <h3>{article.summary}</h3>
+//       </li>
+//     )
+//   })
+
+
+
   return(
     <>
     <h3>ArticleList</h3>
@@ -23,3 +55,6 @@ const ArticleList = (props) => {
 }
 
 export default ArticleList;
+
+
+  // if (article.category == filter.toUpperCase())
