9c84a8e2ba92a4323ae01dabf715efbe96b6ac9f
cjmcauley <christopher.mcauley@gmail.com>
2019-09-30 11:42:21 +0100

    Router removed from AdminContainer

--- client/src/Containers/AdminContainer.js
+++ client/src/Containers/AdminContainer.js
@@ -1,11 +1,8 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import AdminArticles from '../Components/AdminArticles';
 import AdminJournalists from '../Components/AdminJournalists';
 import AdminArticlesForm from '../Components/AdminArticlesForm';
 import AdminJournalistsForm from '../Components/AdminJournalistsForm';
-
-
 
 class AdminContainer extends Component {
 
