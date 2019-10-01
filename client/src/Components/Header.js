6019b39da213dc7f337749d1cf503ae03f7aa33f
cjmcauley <christopher.mcauley@gmail.com>
2019-10-01 13:40:58 +0100

    Categories and Header alignment fixed

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -9,8 +9,8 @@ const Header = () => (
       <Link to="/"><h1 class="header">Newspaper Name</h1></Link>
       </div>
       <div class="buttons">
-      <Link to="/"><h3 class="header">Articles</h3></Link>
-      <Link to="/admin" class="header"><h3>Admin</h3></Link>
+      <Link to="/"><h3 class="header-menu">Articles</h3></Link>
+      <Link to="/admin" class="header-menu"><h3>Admin</h3></Link>
       </div>
     </div>
   </>
