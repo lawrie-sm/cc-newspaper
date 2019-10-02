d975997544768e914e1cd7e7a21ffe0a61bff0e4
cjmcauley <christopher.mcauley@gmail.com>
2019-10-02 11:26:53 +0100

    formatting article details page

--- client/src/Components/Main/ArticleDetail.js
+++ client/src/Components/Main/ArticleDetail.js
@@ -1,24 +1,28 @@
 import React from 'react';
+import Moment from 'react-moment';
 import './ArticleDetail.css';
 
 const ArticleDetail = ({article, imageLink}) => (
-  <>
-    <div>
-    <h1 className="headline">{article.title}</h1>
-    </div>
+    <div class="article-detail-container">
       <div>
-      <h3>{article.date}</h3>
-      <h3>{article.category}</h3>
-      <h3>{article.author.name}, {article.author.title}</h3>
+        <h1 className="headline">{article.title}</h1>
       </div>
-      <div>
-      <h3 className="article-summary">{article.summary}</h3>
-      <h3 className="body">{article.text}</h3>
+        <div className="article-details">
+          <p className="details">{article.author.name}, {article.author.title}</p>
+          <p className="details"><Moment format="dddd, MMMM Do YYYY">{article.date}</Moment></p>
+          <p className="category">{article.category}</p>
+        </div>
+          <h3 className="article-summary">{article.summary}</h3>
+      <div className="article-body">
+        <div>
+        <h3 className="body"><img className="article-image" src={imageLink} width="250" height="auto" align="right" />{article.text}</h3>
+        </div>
       </div>
-    <div>
-    <img src={imageLink} width="250" height="auto" />
     </div>
-  </>
 )
 
 export default ArticleDetail;
+
+// <div>
+// <img className="article-image" src={imageLink} width="250" height="auto" />
+// </div>
