3b4ef5dfc0025b69338ac1b2d750d78c16b18856
cjmcauley <christopher.mcauley@gmail.com>
2019-10-02 09:18:28 +0100

    font sizes and colours changed

--- client/src/Components/Main/ArticleListItem.js
+++ client/src/Components/Main/ArticleListItem.js
@@ -2,15 +2,15 @@ import React from 'react';
 import Moment from 'react-moment';
 import {Link} from 'react-router-dom';
 import './ArticleListItem.css';
-// import culture from '../../Images/culture.jpg';
 
 function importAll(r) {
   return r.keys().map(r);
 }
 
-const images = importAll(require.context('../../Images/', false, /\.(png|jpe?g|svg)$/));
+const images = importAll(require.context('../../Images/', false, /\.jpg$/));
 
 const ArticleListItem = ({article}) => (
+  <>
   <li>
     <div className="article-container">
       <div className="article-list-item-text">
@@ -20,16 +20,17 @@ const ArticleListItem = ({article}) => (
         <div className="article-details">
         <p className="details">{article.author.name}, {article.author.title}</p>
         <p className="details"><Moment fromNow ago>{article.date}</Moment> ago</p>
-        <p className="details">{article.category}</p>
+        <p className="category">{article.category}</p>
         </div>
       <h3 className="summary">{article.summary}</h3>
       </div>
       <div className="article-list-item-image">
-    <img src={images} width="250" height="auto" />
+    <img src={images[0]} width="250" height="auto" />
       </div>
     </div>
-  <hr />
   </li>
+    <hr align="left" />
+    </>
 )
 
 export default ArticleListItem;
